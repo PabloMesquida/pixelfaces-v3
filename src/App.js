1f3b8203ddcf88bb755d4e1a3b07d34f29cfa916
PabloMesquida <pablomesquida67@gmail.com>
2022-11-08 20:47:38 +0100

    pushVerce

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "styled-components";
 import { Theme } from "./Theme.js";
 import { Container } from "./App.styles.js";
 import Loader from "./components/Loader.js";
+
 const Main = React.lazy(() => import("./components/main/Main.js"));
 const Site = React.lazy(() => import("./Site.js"));
 
