2f2139be670964cb1f62e286c185a256ae3c6246
PabloMesquida <pablomesquida67@gmail.com>
2022-11-08 20:03:04 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { Suspense } from "react";
 import { GlobalStyle } from "./GlobalStyles.js";
 import { ThemeProvider } from "styled-components";
 import { Theme } from "./Theme.js";
 import { Container } from "./App.styles.js";
-import Main from "./components/main/Main.js";
-import Site from "./Site.js";
+import Loader from "./components/Loader.js";
+
+const Main = React.lazy(() => import("./components/main/Main.js"));
+const Site = React.lazy(() => import("./Site.js"));
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
       <Container>
-        <Main />
-        <Site />
+        <Suspense fallback={<Loader />}>
+          <Main />
+          <Site />
+        </Suspense>
       </Container>
     </ThemeProvider>
   );
