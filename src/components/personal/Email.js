d529a6c5d8ac6c47704175d0169b3180d10fe11c
PabloMesquida <pablomesquida67@gmail.com>
2022-11-09 22:18:35 +0100

    email

--- src/components/personal/Email.js
+++ src/components/personal/Email.js
@@ -7,7 +7,7 @@ const Email = () => {
     <PCont>
       <EmailTxt>
         <IconMail w={16} h={16} />
-        <a href="mailto:pablo@thepixelfaces.com">pablo@thepixelfaces.com</a>
+        <a href="mailto:pablomesquida67@gmail.com">pablomesquida67@gmail.com</a>
       </EmailTxt>
     </PCont>
   );
